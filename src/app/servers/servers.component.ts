c55ddc71e45bd2392ad046c415bfdd19117486ee
Justin Hornsby <justin.hornsby@entelect.co.za>
2022-11-30 13:28:50 +0200

    add ngFor functionality

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent {
   serverCreationStatus: string = "no server was created";
   serverName: string ="";
   serverCreated: boolean = false;
+  servers = ["Testserver", "Testserver 2"]
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
 
